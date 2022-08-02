6a2a520bbd60e620de4f711c87e9e1c729b992af
Gehua <tonggehua18@gmail.com>
2022-08-02 09:25:26 -0400

    Finalize game 1 questions

--- app/questions.py
+++ app/questions.py
@@ -5,6 +5,7 @@ from app import db
 from models import MultipleChoice
 import random
 
+
 # Game 1 questions
 # Try initiating some MC questions!
 def initialize_game1_questions():
@@ -16,8 +17,32 @@ def initialize_game1_questions():
         List of MultipleChoice database models for game 1
     """
     mcs = [] # List of questions
-
     mcs.append(MultipleChoice(
+        id=101,
+        game_number=1,
+        uses_images=False,
+        question_text="What combination of matrix size and FOV gives you the highest resolution?",
+        choiceA = "Large matrix size and small FOV",
+        choiceB = "Small matrix size and large FOV",
+        choiceC = "Large matrix size and large FOV",
+        choiceD = "Small matrix size and small FOV",
+        correct_choice = "A",
+        difficulty='easy'
+    ))
+    mcs.append(MultipleChoice(
+        id=102,
+        game_number=1,
+        uses_images=False,
+        question_text="What happens as the zero-fill value is increased?",
+        choiceA="The matrix size increases",
+        choiceB = "The matrix size decreases",
+        choiceC = "The matrix size stays the same",
+        choiceD = "The resolution is improved",
+        correct_choice="A",
+        difficulty="medium"
+    ))
+    mcs.append(MultipleChoice(
+        id=103,
         game_number=1,
         uses_images=False,
         question_text="What combination of matrix size and FOV gives you the highest resolution?",
@@ -30,6 +55,7 @@ def initialize_game1_questions():
     ))
 
     mcs.append(MultipleChoice(
+        id=104,
         game_number=1,
         uses_images=False,
         question_text="Why does acquiring smaller pixels lead to better resolution?",
@@ -42,6 +68,7 @@ def initialize_game1_questions():
     ))
 
     mcs.append(MultipleChoice(
+        id=105,
         game_number=1,
         uses_images=False,
         question_text="What min/max window settings would allow the most visual contrast to show up between the two circles (numbers indicate gray level)?",
@@ -55,6 +82,7 @@ def initialize_game1_questions():
     ))
 
     mcs.append(MultipleChoice(
+        id=106,
         game_number=1,
         uses_images=True,
         question_text="Which image has the highest resolution?",
@@ -67,6 +95,7 @@ def initialize_game1_questions():
     ))
 
     mcs.append(MultipleChoice(
+        id=107,
         game_number=1,
         uses_images=True,
         question_text="Which image has the tiniest FOV?",
@@ -79,6 +108,7 @@ def initialize_game1_questions():
     ))
 
     mcs.append(MultipleChoice(
+        id=108,
         game_number=1,
         uses_images=True,
         question_text="Which image has the smallest matrix size?",
@@ -91,6 +121,7 @@ def initialize_game1_questions():
     ))
 
     mcs.append(MultipleChoice(
+        id=109,
         game_number=1,
         uses_images=False,
         question_text="What is the difference between a pixel and a voxel?",
@@ -103,6 +134,7 @@ def initialize_game1_questions():
     ))
 
     mcs.append(MultipleChoice(
+        id=110,
         game_number=1,
         uses_images=False,
         question_text="What does zero-filling this reduced matrix size image of a pineapple do?",
@@ -118,7 +150,7 @@ def initialize_game1_questions():
     return mcs
 
 def refresh_database_and_add_questions(mcs):
-    #db.drop_all() # Gets rid of database structure as well
+    db.drop_all() # Gets rid of database structure as well
     db.create_all()# Re-create tables
     for mc in mcs:
         db.session.add(mc)
@@ -145,31 +177,43 @@ if __name__ == "__main__":
     #           if you need to modify the question, delete the local file myDB.db first.
     #                     It gets regenerated when you call the function db.create_all().
 
-
     # Input questions into database
-    initialize_game1_questions()
+    from models import initialize_users
+
+    mc_list = initialize_game1_questions()
+    refresh_database_and_add_questions(mc_list)
+    initialize_users() # adds admin back into database
+
+    # Get first and second question for unlocking purposes
+    # First question
+    print(MultipleChoice.query.get(101).get_randomized_data())
+    # Second question
+    print(MultipleChoice.query.get(102).get_randomized_data())
+
+
     # Example querying of database
     # All Game 1 questions
 
     # Get all game 1 questions
-    print('All questions for game 1: ')
-    q = MultipleChoice.query.filter_by(game_number=1).all()
-    print(q)
+    # print('All questions for game 1: ')
+    # q = MultipleChoice.query.filter_by(game_number=1).all()
+    # print(q)
+    #
+    # # Get the first game 1 question that is easy
+    # print('The first easy question for game 1: ')
+    # q2 = MultipleChoice.query.filter_by(game_number=1,difficulty='easy').first_or_404() # Only gets first!!
+    # print(q2)
+    #
+    # # Get one random question (game number not specified)
+    # print('Random question from any game: ')
+    # q3 = MultipleChoice.query.all()
+    # print(random.sample(q3,1))
+    #
+    # # Check answer
+    # my_question = q2
+    # # Check answer by text
+    # print(my_question.check_answer('Pixel amount and size have no effect on resolution'))
+    #
+    # # Shuffle A,B,C,D choices
+    # print(my_question.get_randomized_data())
 
-    # Get the first game 1 question that is easy
-    print('The first easy question for game 1: ')
-    q2 = MultipleChoice.query.filter_by(game_number=1,difficulty='easy').first_or_404() # Only gets first!!
-    print(q2)
-
-    # Get one random question (game number not specified)
-    print('Random question from any game: ')
-    q3 = MultipleChoice.query.all()
-    print(random.sample(q3,1))
-
-    # Check answer
-    my_question = q2
-    # Check answer by text
-    print(my_question.check_answer('Pixel amount and size have no effect on resolution'))
-
-    # Shuffle A,B,C,D choices
-    print(my_question.get_randomized_data())
